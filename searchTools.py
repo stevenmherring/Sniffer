8c59c89698232e2146f52e60cbe56022e542506c
Steven M Herring <stevenmherring@gmail.com>
2016-10-24 14:45:19 -0400

    optimized search feature to short circuit and avoid double writes

--- searchTools.py
+++ searchTools.py
@@ -1,5 +1,10 @@
+import re
 results = "search_results.log" #out log for search packets
 writePacket = False #flag
+#read the dump line by line, recreating each individual packet by the
+#Packet Number:
+#if any line contains the regex/term, set a flag
+#if flag is true we write the entire packet
 def searchPackets(infile, term, packetID):
     idlength = len(packetID)
     try:
@@ -15,19 +20,13 @@ def searchPackets(infile, term, packetID):
                 if packetID == checkLine:
                     #start of a new packet
                     #check if former packet held search term
-                    if writePacket == True:
+                    if currentPacket != "" and bool(re.search(term, currentPacket)):
                         try:
                             outfile.write(currentPacket + "\n")
                         except IOError as err:
                             print (str(err))
                     #start recording next packet
                     currentPacket = line
-                    #set flag back to flase
-                    writePacket = False
-                elif bool(re.search(term, line)):
-                    #packet has search term, mark as such
-                    writePacket = True
-                    currentPacket = currentPacket + "\n" + line #append line onto currentPacket
                 else:
                     currentPacket = currentPacket + "\n" + line #append line onto currentPacket
     except IOError as err:
